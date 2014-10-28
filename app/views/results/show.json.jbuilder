7d27fd50b96439b286ec76d33e7879ebde2a6658
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-10-28 12:41:36 +0000

    Results table

---
+++ app/views/results/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @result, :id, :url, :image, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027165522) do
+ActiveRecord::Schema.define(version: 20141028123955) do
+
+  create_table "results", force: true do |t|
+    t.string   "url"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "searches", force: true do |t|
     t.string   "colour"
